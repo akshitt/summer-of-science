160497c5d268c47380356f829f571062edde5cf7
akshitt <akshit.srivastava99@gmail.com>
2019-06-14 11:54:56 +0530

    Mid-summers sorted

---
+++ titanic/titanic.py
@@ -0,0 +1,189 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+# %matplotlib inline
+import seaborn as sns
+sns.set() # setting seaborn default for plots
+
+#!----------------------------------------------------------------------------------------
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC, LinearSVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import Perceptron
+from sklearn.linear_model import SGDClassifier
+
+#!-----------------------------------------------------------------------------------------
+
+train = pd.read_csv('train.csv')
+test = pd.read_csv('test.csv')
+
+
+
+train_test_data = [train, test]
+
+#!------------------------------------------------------------------------------------------
+
+survived = train[train['Survived'] == 1]
+not_survived = train[train['Survived'] == 0]
+
+train.Pclass.value_counts()
+train.groupby('Pclass').Survived.value_counts()
+
+train[['Pclass', 'Survived']].groupby(['Pclass'], as_index=False).mean()
+
+sns.barplot(x='Pclass', y='Survived', data=train)
+
+train.groupby('Sex').Survived.value_counts()
+
+train[['Sex', 'Survived']].groupby(['Sex'], as_index=False).mean()
+
+tab = pd.crosstab(train['Pclass'], train['Sex'])
+# print (tab)
+
+tab.div(tab.sum(1).astype(float), axis=0).plot(kind="bar", stacked=True)
+plt.xlabel('Pclass')
+plt.ylabel('Percentage')
+sns.catplot('Sex', 'Survived', hue='Pclass', height=4, aspect=2, data=train)
+
+train.groupby('Embarked').Survived.value_counts()
+train[['Embarked', 'Survived']].groupby(['Embarked'], as_index=False).mean()
+
+train.Parch.value_counts()
+train.groupby('Parch').Survived.value_counts()
+train[['Parch', 'Survived']].groupby(['Parch'], as_index=False).mean()
+train.SibSp.value_counts()
+
+train.groupby('SibSp').Survived.value_counts()
+train[['SibSp', 'Survived']].groupby(['SibSp'], as_index=False).mean()
+
+
+for dataset in train_test_data:
+    dataset['Title'] = dataset.Name.str.extract(' ([A-Za-z]+)\.')
+
+pd.crosstab(train['Title'], train['Sex'])
+
+#!------------------------------------------------------------------------------------------
+# sns.factorplot('Sex', 'Survived', hue='Pclass', size=4, aspect=2, data=train)
+
+
+for dataset in train_test_data:
+    dataset['Title'] = dataset['Title'].replace(['Lady', 'Countess','Capt', 'Col', \
+ 	'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Other')
+
+    dataset['Title'] = dataset['Title'].replace('Mlle', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Ms', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Mme', 'Mrs')
+    
+
+for dataset in train_test_data:
+    dataset['Sex'] = dataset['Sex'].map( {'female': 1, 'male': 0} ).astype(int)
+
+for dataset in train_test_data:
+    dataset.loc[ dataset['Age'] <= 16, 'Age'] = 0
+    dataset.loc[(dataset['Age'] > 16) & (dataset['Age'] <= 32), 'Age'] = 1
+    dataset.loc[(dataset['Age'] > 32) & (dataset['Age'] <= 48), 'Age'] = 2
+    dataset.loc[(dataset['Age'] > 48) & (dataset['Age'] <= 64), 'Age'] = 3
+    dataset.loc[ dataset['Age'] > 64, 'Age'] = 4
+
+title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Other": 5}
+for dataset in train_test_data:
+    dataset['Title'] = dataset['Title'].map(title_mapping)
+    dataset['Title'] = dataset['Title'].fillna(0)
+
+# for dataset in train_test_data:
+#     dataset['Sex'] = dataset['Sex'].map( {'female': 1, 'male': 0} ).astype(int)
+
+train.Embarked.unique()
+train.Embarked.value_counts()
+
+for dataset in train_test_data:
+    dataset['Embarked'] = dataset['Embarked'].fillna('S')
+
+train.head()
+
+
+for dataset in train_test_data:
+    print(dataset.Embarked.unique())
+    dataset['Embarked'] = dataset['Embarked'].map( {'S': 0, 'C': 1, 'Q': 2} ).astype(int)
+
+
+for dataset in train_test_data:
+    age_avg = dataset['Age'].mean()
+    age_std = dataset['Age'].std()
+    age_null_count = dataset['Age'].isnull().sum()
+    
+    age_null_random_list = np.random.randint(age_avg - age_std, age_avg + age_std, size=age_null_count)
+    dataset['Age'][np.isnan(dataset['Age'])] = age_null_random_list
+    dataset['Age'] = dataset['Age'].astype(int)
+    
+train['AgeBand'] = pd.cut(train['Age'], 5)
+
+
+for dataset in train_test_data:
+    dataset.loc[ dataset['Age'] <= 16, 'Age'] = 0
+    dataset.loc[(dataset['Age'] > 16) & (dataset['Age'] <= 32), 'Age'] = 1
+    dataset.loc[(dataset['Age'] > 32) & (dataset['Age'] <= 48), 'Age'] = 2
+    dataset.loc[(dataset['Age'] > 48) & (dataset['Age'] <= 64), 'Age'] = 3
+    dataset.loc[ dataset['Age'] > 64, 'Age'] = 4
+
+for dataset in train_test_data:
+    dataset['Fare'] = dataset['Fare'].fillna(train['Fare'].median())
+
+train['FareBand'] = pd.qcut(train['Fare'], 4)
+# print (train[['FareBand', 'Survived']].groupby(['FareBand'], as_index=False).mean())
+
+
+for dataset in train_test_data:
+    dataset.loc[ dataset['Fare'] <= 7.91, 'Fare'] = 0
+    dataset.loc[(dataset['Fare'] > 7.91) & (dataset['Fare'] <= 14.454), 'Fare'] = 1
+    dataset.loc[(dataset['Fare'] > 14.454) & (dataset['Fare'] <= 31), 'Fare']   = 2
+    dataset.loc[ dataset['Fare'] > 31, 'Fare'] = 3
+    dataset['Fare'] = dataset['Fare'].astype(int)
+
+
+for dataset in train_test_data:
+    dataset['FamilySize'] = dataset['SibSp'] +  dataset['Parch'] + 1
+
+
+for dataset in train_test_data:
+    dataset['IsAlone'] = 0
+    dataset.loc[dataset['FamilySize'] == 1, 'IsAlone'] = 1
+    
+
+features_drop = ['Name', 'SibSp', 'Parch', 'Ticket', 'Cabin', 'FamilySize']
+train = train.drop(features_drop, axis=1)
+test = test.drop(features_drop, axis=1)
+train = train.drop(['PassengerId', 'AgeBand', 'FareBand'], axis=1)
+
+
+# print (train[['IsAlone', 'Survived']].groupby(['IsAlone'], as_index=False).mean())
+
+# print (train[['FamilySize', 'Survived']].groupby(['FamilySize'], as_index=False).mean())
+
+#!-----------------------------------------------------------------------------------------------
+
+X_train = train.drop('Survived', axis=1)
+y_train = train['Survived']
+X_test = test.drop("PassengerId", axis=1).copy()
+
+clf = DecisionTreeClassifier
+clf.fit(X_train, y_train)
+y_pred_random_forest = clf.predict(X_test)
+acc_random_forest = round(clf.score(X_train, y_train) * 100, 2)
+print (acc_random_forest)
+
+#!------------------------------------------------------------------------------------------------
+
+submission = pd.DataFrame({
+        "PassengerId": test["PassengerId"],
+        "Survived": y_pred_random_forest
+    })
+
+submission.to_csv('submission.csv', index=False)
+
+# a =  np.genfromtxt(train)
+# print()
